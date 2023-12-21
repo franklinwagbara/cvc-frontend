d31f6faed0c4b8b18829f2358f09ae664ff3b3d3
Adeniran Olukanni <adeniran.olukanni@brandonetech.com>
2023-12-21 08:32:03 +0100

    Add noa application models

---
+++ src/app/shared/interfaces/IAppDepot.ts
@@ -0,0 +1,8 @@
+export interface IAppDepot {
+  id?: number;
+  depotId?: number;
+  appId?: number | null;
+  name?: string | null;
+  productId?: number;
+  volume?: number;
+}
--- src/app/shared/interfaces/IApplication.ts
+++ src/app/shared/interfaces/IApplication.ts
@@ -1,53 +1,19 @@
-import { IVessel } from 'src/app/company/apply/new-application/new-application.component';
-import { IAppHistory } from './IAppHistory';
-import { IApplicationForm } from './IApplicationForm';
-import { IExtraPayment } from './IExtraPayment';
-import { IInspectionForm } from './IInspectionForm';
-import { ISchedule } from './ISchedule';
+import { IAppDepot } from "./IAppDepot";
+import { ITank } from "./ITank";
+
 
 export interface IApplication {
-  id: number;
-  addedDate: Date;
-  appHistories: IAppHistory[];
-  reference: string;
-  appType: string;
-  companyName: string;
-  email: string;
-  facilityAddress: string;
-  currentDesk: string;
-  companyAddress: string;
-  facilityName: string;
-  facilityType: string;
-  lga: string;
-  paymentDescription: string;
-  paymnetDate: string;
-  paymnetStatus: string;
-  currentUser: string;
-  schedules: ISchedule[];
-  state: string;
-  status: boolean;
-  submittedDate: string;
-  totalAmount: number;
-  createdDate: string;
-
-  extraPayments: IExtraPayment[];
-  gpsCordinates: string;
-  inspectionForm: IInspectionForm[];
-  permitType: string;
-  rrr: string;
-  lgaId: number;
-  location: string;
-  phaseName: string;
-  phaseStageId: number;
-  stateId: number;
-  applicationDocs: IDoc[];
-
-  applicationforms: IApplicationForm[];
-  category: string;
-  categoryCode: string;
-  categoryId: string;
-
-  vessel: IVessel;
+  applicationTypeId?: number;
+  vesselTypeId?: number;
+  imoNumber?: string | null;
+  vesselName?: string | null;
+  loadingPort?: string | null;
+  dischargePort?: string | null;
+  marketerName?: string | null;
+  deportStateId?: number;
+  tankList?: Array<ITank> | null;
+  depotList?: Array<IAppDepot> | null;
+  eta?: string;
 }
 
 export interface IDoc {
