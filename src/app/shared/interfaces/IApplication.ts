0088a67fdb435be7d75f66e558f1153f3edbdbff
Adeniran Olukanni <adeniran.olukanni@brandonetech.com>
2023-12-21 08:38:37 +0100

    Revert file

--- src/app/shared/interfaces/IApplication.ts
+++ src/app/shared/interfaces/IApplication.ts
@@ -1,19 +1,53 @@
-import { IAppDepot } from "./IAppDepot";
-import { ITank } from "./ITank";
-
+import { IVessel } from 'src/app/company/apply/new-application/new-application.component';
+import { IAppHistory } from './IAppHistory';
+import { IApplicationForm } from './IApplicationForm';
+import { IExtraPayment } from './IExtraPayment';
+import { IInspectionForm } from './IInspectionForm';
+import { ISchedule } from './ISchedule';
 
 export interface IApplication {
-  applicationTypeId?: number;
-  vesselTypeId?: number;
-  imoNumber?: string | null;
-  vesselName?: string | null;
-  loadingPort?: string | null;
-  dischargePort?: string | null;
-  marketerName?: string | null;
-  deportStateId?: number;
-  tankList?: Array<ITank> | null;
-  depotList?: Array<IAppDepot> | null;
-  eta?: string;
+  id: number;
+  addedDate: Date;
+  appHistories: IAppHistory[];
+  reference: string;
+  appType: string;
+  companyName: string;
+  email: string;
+  facilityAddress: string;
+  currentDesk: string;
+  companyAddress: string;
+  facilityName: string;
+  facilityType: string;
+  lga: string;
+  paymentDescription: string;
+  paymnetDate: string;
+  paymnetStatus: string;
+  currentUser: string;
+  schedules: ISchedule[];
+  state: string;
+  status: boolean;
+  submittedDate: string;
+  totalAmount: number;
+  createdDate: string;
+
+  extraPayments: IExtraPayment[];
+  gpsCordinates: string;
+  inspectionForm: IInspectionForm[];
+  permitType: string;
+  rrr: string;
+  lgaId: number;
+  location: string;
+  phaseName: string;
+  phaseStageId: number;
+  stateId: number;
+  applicationDocs: IDoc[];
+
+  applicationforms: IApplicationForm[];
+  category: string;
+  categoryCode: string;
+  categoryId: string;
+
+  vessel: IVessel;
 }
 
 export interface IDoc {
