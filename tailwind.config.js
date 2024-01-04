d12c767d584ea699cef94bdf144f04977fad732e
Adeniran Olukanni <adeniran.olukanni@brandonetech.com>
2024-01-04 13:39:46 +0100

    minor update

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,10 +41,10 @@ module.exports = {
       },
     },
   },
-  plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
-  }
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss/nesting'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
 };
