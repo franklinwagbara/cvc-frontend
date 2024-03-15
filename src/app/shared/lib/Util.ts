273f19325655ab77706e8c4d968f7d66da7dd5a1
Adeniran Olukanni <adeniran.olukanni@brandonetech.com>
2024-03-15 09:41:33 +0100

    fix pdf export

--- src/app/shared/lib/Util.ts
+++ src/app/shared/lib/Util.ts
@@ -77,7 +77,7 @@ export class Util {
         html2canvas:  { scale: 2 },
         jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
       };
-      saveAs(html2pdf(table, opt), filename || 'data.pdf')
+      html2pdf(table, opt);
     } else if (format === "csv") {
       let csv = []
       for (const row of Array.from(rows)) {
